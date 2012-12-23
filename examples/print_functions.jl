f84ad953e3fc84a7cf01716a5971d1ad97d0e7a7
Isaiah <isaiah.norton@gmail.com>
2012-12-23 16:19:57 -0500

    Update print_functions example

--- examples/print_functions.jl
+++ examples/print_functions.jl
@@ -4,21 +4,27 @@ import cindex.CurKind
 import cindex.TypKind
 
 function full_name(t::CXType)
+  
+  # Get the real return type through
+  # typedefs and pointers. Some of this
+  # is not fully exposed by libclang so
+  # some extra steps are needed.
+
   fn = {}
   tdecl = cindex.getTypeDeclaration(t)
-  if kind(t) == TypKind.ENUM
+  if ty_kind(t) == TypKind.ENUM
     push(fn, string(spelling(tdecl)) )
-  elseif kind(t) == TypKind.TYPEDEF
+  elseif ty_kind(t) == TypKind.TYPEDEF
     td = cindex.getTypedefDeclUnderlyingType(tdecl)
-    if (kind(td) == TypKind.UNEXPOSED) td=resolve_type(td) end
+    if (ty_kind(td) == TypKind.UNEXPOSED) td=resolve_type(td) end
     push(fn, string(spelling(td), " ", name(tdecl)))
-  elseif kind(t) == TypKind.POINTER
+  elseif ty_kind(t) == TypKind.POINTER
     pt = cindex.getPointeeType(t)
     push(fn, string( spelling(pt) ))
-    if kind(pt) == TypKind.TYPEDEF
+    if ty_kind(pt) == TypKind.TYPEDEF
       tdecl = cindex.getTypeDeclaration(pt)
       td = cindex.getTypedefDeclUnderlyingType(tdecl)
-      if (kind(td) == TypKind.UNEXPOSED) td=resolve_type(td) end
+      if (ty_kind(td) == TypKind.UNEXPOSED) td=resolve_type(td) end
       push(fn, full_name(td) )
       push(fn, spelling(tdecl) )
     end
@@ -32,12 +38,13 @@ function dump_functions(cl::CursorList, restrict_hdr::Any)
     if (restrict_hdr != None && cu_file(cu) != restrict_hdr)
       continue
     end
-    if kind(cu) == CurKind.FUNCTIONDECL
+    if cu_kind(cu) == CurKind.FUNCTIONDECL
       rt = return_type(cu)
       println(i, " ", full_name(rt), " ", name(cu))
     end
   end
 end
+
 function dump_functions(hdr::String)
   tu = tu_init(hdr)
   topcu = tu_cursor(tu)

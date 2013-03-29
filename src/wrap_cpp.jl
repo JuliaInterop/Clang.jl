9941819ae884e30484ba36c4542a406cbdb72240
Isaiah <isaiah.norton@gmail.com>
2013-03-28 21:47:39 -0400

    update: working vcall macro

--- src/wrap_cpp.jl
+++ src/wrap_cpp.jl
@@ -17,13 +17,20 @@ end
 # Get function pointer from vtable at given offset
 vtblfunc(p::Ptr{Void}, offset) = pointer(Void, unsafe_ref(pointer(Uint64, unsafe_ref(pointer(Uint64,p))))+offset )
 
+#derefptr(p::Ptr{Void}, offset) = convert( Ptr{Void}, pointer_to_array(convert(Ptr{Uint64}, p+offset), (1,) )[1] )
+derefptr(thisptr, vtidx) = pointer(Void, unsafe_ref(pointer(Uint64, unsafe_ref(pointer(Uint64,thisptr)))+vtidx*8) )
+
 macro vcall(vtidx, ret_type, func, arg_types)
   local _args_in = Any[ symbol(string('a',x)) for x in 1:length(arg_types.args) ]
-  local _arg_types = tuple(:(Ptr{Void}), arg_types.args...)
+  larg_types = :((Ptr{Void}, $(arg_types.args...)))
+  println("argi: ", arg_types)
+  println("argt: ", larg_types)
   quote
     function $(esc(func))(thisptr, $(_args_in...))
-      fptr = pointer(Void, unsafe_ref(pointer(Uint64, unsafe_ref(pointer(Uint64,thisptr))))+$(vtidx) )
-      ccall( fptr, thiscall, $ret_type, $arg_types, $(_args_in...) )
+      local fptr = derefptr(thisptr, $(vtidx) )
+      println("fptr: ", fptr, "\n")
+      ccall( fptr, thiscall, $(esc(ret_type)), $(larg_types), thisptr, $(_args_in...) )
+      #$(esc( Expr(:ccall, ret_type, larg_types, :thisptr, _args_in..., :thiscall)) )
     end
   end
 end
@@ -31,7 +38,8 @@ end
 macro scall(ret_type, func, arg_types, sym, lib)
   local _args_in = Any[ symbol(string('a',x)) for x in 1:length(arg_types.args) ]
   hdl = dlopen(string(lib))
-  fptr = dlsym(hdl, sym)
+  fptr = dlsym_e(hdl, sym)
+  if (fptr==C_NULL) return end
   quote
     function $(esc(func))($(_args_in...))
       ccall( $(esc(fptr)), $ret_type, $arg_types, $(_args_in...) )
@@ -41,7 +49,6 @@ end
 
 function find_sym(name,liblist)
   libs = {dlopen(l)=>l for l in liblist}
-  println(libs)
   found = false
   dl = C_NULL
   for dl in keys(libs)
@@ -54,7 +61,7 @@ function find_sym(name,liblist)
   end
   if found
     println("symbol $name found in ", libs[dl])
-    return libs[dl]
+    return splitdir(libs[dl])[end]
   else
     return None
     println("NOT FOUND")

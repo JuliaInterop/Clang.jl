1b3f3967347cff8d3e5821d7cf1773d0a16caffa
Isaiah <isaiah.norton@gmail.com>
2013-05-11 12:53:14 -0400

    Make C++ support optional based on USE_CLANG_CPP
    Variable should be set in ENV for Makefile
    Also should be set in julia before importing Clang.jl

--- deps/src/wrapclang.cpp
+++ deps/src/wrapclang.cpp
@@ -23,7 +23,81 @@ wci_st(CXType)
 wci_st(CXToken)
 wci_st(CXString)
 
-// Test code
+typedef std::vector<CXCursor> CursorList;
+typedef std::set<CursorList*> allcl_t;
+allcl_t allCursorLists;
+
+// to traverse AST with cursor visitor
+// TODO: replace this with a C container
+CXChildVisitResult wci_visitCB(CXCursor cur, CXCursor par, CXClientData data)
+{
+  CursorList* cl = (CursorList*)data;
+  cl->push_back(cur);
+  return CXChildVisit_Continue;
+}
+
+extern "C" {
+#include "wrapclang.h"
+
+unsigned int wci_getChildren(char* cuin, CursorList* cl)
+{
+  CXCursor cu = wci_get_CXCursor(cuin);
+  clang_visitChildren(cu, wci_visitCB, cl);
+  return 0;
+}
+
+void wci_getCursorFile(char* cuin, char* cxsout)
+{
+  CXCursor cu = wci_get_CXCursor(cuin);
+  CXSourceLocation loc = clang_getCursorLocation( cu );
+  CXFile cxfile;
+  clang_getExpansionLocation(loc, &cxfile, 0, 0, 0);
+  wci_save_CXString(clang_getFileName(cxfile), cxsout);
+}
+
+CursorList* wci_createCursorList()
+{
+  CursorList* cl = new CursorList();
+  allCursorLists.insert(cl);
+  return cl;
+}
+
+void wci_disposeCursorList(CursorList* cl)
+{
+  cl->clear();
+  allCursorLists.erase(cl);
+}
+
+unsigned int wci_sizeofCursorList(CursorList* cl)
+{
+  return cl->size();
+}
+
+void wci_getCLCursor(char* cuout, CursorList* cl, int cuid)
+{
+  wci_save_CXCursor((*cl)[cuid], cuout);
+}
+
+const char* wci_getCString(char* csin )
+{
+  CXString cxstr = wci_get_CXString(csin);
+  return clang_getCString(cxstr);
+}
+
+void wci_disposeString(char* csin)
+{
+  CXString cxstr = wci_get_CXString(csin);
+  clang_disposeString(cxstr);
+}
+
+} // extern
+
+/* 
+  Optional Clang.jl C++ Support                                               #
+*/
+
+#ifdef USE_CLANG_CPP
+
 #define __STDC_LIMIT_MACROS
 #define __STDC_CONSTANT_MACROS
 #include "clang/AST/ASTContext.h"
@@ -143,72 +217,9 @@ int wci_getCXXClassParents(char* cuin, ClassParentCB visit_cb, void* cbdata)
 #undef __STDC_LIMIT_MACROS
 #undef __STDC_CONSTANT_MACROS
 
+#endif
+/*
+  End of Optional Clang.jl C++ Support
+*/
 
-typedef std::vector<CXCursor> CursorList;
-typedef std::set<CursorList*> allcl_t;
-allcl_t allCursorLists;
 
-// to traverse AST with cursor visitor
-// TODO: replace this with a C container
-CXChildVisitResult wci_visitCB(CXCursor cur, CXCursor par, CXClientData data)
-{
-  CursorList* cl = (CursorList*)data;
-  cl->push_back(cur);
-  return CXChildVisit_Continue;
-}
-
-extern "C" {
-#include "wrapclang.h"
-
-unsigned int wci_getChildren(char* cuin, CursorList* cl)
-{
-  CXCursor cu = wci_get_CXCursor(cuin);
-  clang_visitChildren(cu, wci_visitCB, cl);
-  return 0;
-}
-
-void wci_getCursorFile(char* cuin, char* cxsout)
-{
-  CXCursor cu = wci_get_CXCursor(cuin);
-  CXSourceLocation loc = clang_getCursorLocation( cu );
-  CXFile cxfile;
-  clang_getExpansionLocation(loc, &cxfile, 0, 0, 0);
-  wci_save_CXString(clang_getFileName(cxfile), cxsout);
-}
-
-CursorList* wci_createCursorList()
-{
-  CursorList* cl = new CursorList();
-  allCursorLists.insert(cl);
-  return cl;
-}
-
-void wci_disposeCursorList(CursorList* cl)
-{
-  cl->clear();
-  allCursorLists.erase(cl);
-}
-
-unsigned int wci_sizeofCursorList(CursorList* cl)
-{
-  return cl->size();
-}
-
-void wci_getCLCursor(char* cuout, CursorList* cl, int cuid)
-{
-  wci_save_CXCursor((*cl)[cuid], cuout);
-}
-
-const char* wci_getCString(char* csin )
-{
-  CXString cxstr = wci_get_CXString(csin);
-  return clang_getCString(cxstr);
-}
-
-void wci_disposeString(char* csin)
-{
-  CXString cxstr = wci_get_CXString(csin);
-  clang_disposeString(cxstr);
-}
-
-} // extern

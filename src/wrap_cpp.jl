cfeff856e147f81e7b0c2c70434963c299794397
Isaiah Norton <isaiah.norton@gmail.com>
2013-08-21 21:57:43 -0400

    Remove wrapper macros from wrap_cpp

--- src/wrap_cpp.jl
+++ src/wrap_cpp.jl
@@ -34,48 +34,6 @@ function base_class(cursor::cindex.CXCursor)
     return temp[1] # TODO: don't assume single inheritance...
 end
 
-# Get function pointer from vtable at given offset
-#derefptr(thisptr::Ptr{Void}, vtidx) = unsafe_ref(unsafe_ref(pointer(Ptr{Ptr{Void}},thisptr)), vtidx)
-
-# Static method call
-macro scall(ret_type, func, arg_types, sym, lib)
-    local _args_in = Any[ symbol(string('a',x)) for x in 1:length(arg_types.args) ]
-    hdl = dlopen(string(lib))
-    fptr = dlsym_e(hdl, sym)
-    if (fptr==C_NULL) return end
-    quote
-        function $(esc(func))($(_args_in...))
-            ccall( $(esc(fptr)), $(esc(ret_type)), $(esc(arg_types)), $(_args_in...) )
-        end
-    end
-end
-
-# Member function call (takes this* but is not virtual)
-macro mcall(ret_type, func, arg_types, sym, lib)
-    local _args_in = Any[ symbol(string('a',x)) for x in 1:length(arg_types.args) ]
-    larg_types = :((Ptr{Void}, $(arg_types.args...)))
-    hdl = dlopen(string(lib))
-    fptr = dlsym_e(hdl,sym)
-    quote
-        function $(esc(func))(thisptr, $(_args_in...))
-            ccall( $(fptr), thiscall, $(esc(ret_type)), $(esc(larg_types)), thisptr, $(_args_in...) )
-        end
-    end
-end
-
-# Virtual table call
-macro vcall(vtidx, ret_type, func, arg_types, classname)
-    local _args_in = Any[ symbol(string('a',x)) for x in 1:length(arg_types.args) ]
-    larg_types = :((Ptr{$(classname)}, $(arg_types.args...)))
-    quote
-        function $(esc(func)){T <: $(esc(classname))}(thisptr::Ptr{T}, $(_args_in...))
-            local fptr =    unsafe_ref(unsafe_ref(pointer(Ptr{Ptr{Void}},thisptr)), $(vtidx)+1)
-            println("fptr: ", fptr, "\n")
-            ccall( fptr, thiscall, $(esc(ret_type)), $(esc(larg_types)), thisptr, $(_args_in...) )
-        end
-    end
-end
-
 function find_sym(name,liblist)
     libs = {dlopen(l)=>l for l in liblist}
     found = false

4d7560e6afba7fe1594e8809b7654cf1f536540c
Isaiah Norton <isaiah.norton@gmail.com>
2013-09-23 19:32:24 -0400

    Move utility functions to util.jl from wrap_cpp.jl

--- src/wrap_cpp.jl
+++ src/wrap_cpp.jl
@@ -3,50 +3,7 @@ module wrap_cpp
 using Clang.cindex
 
 function base_classes(c)
-	search(c, isa(c, CursorKind.CXXBaseSpecifier))
-end
-
-function find_sym(name,liblist)
-    libs = {dlopen(l)=>l for l in liblist}
-    found = false
-    dl = C_NULL
-    for dl in keys(libs)
-        try
-            dlsym_e(dl, name)
-        catch
-            continue
-        end
-        found = true
-    end
-    if found
-        println("symbol $name found in ", libs[dl])
-        return splitdir(libs[dl])[end]
-    else
-        return None
-        println("NOT FOUND")
-    end
-end
-
-###############################################################################
-# Extra helpers
-#	These functions require libwrapclang to be compiled with
-#	-DUSE_CLANG_CPP. They provide some extra information directly
-#	from the Clang C++ API.
-###############################################################################
-
-function method_vt_index(cursor::cindex.CXCursor)
-    if !(cindex.CXXMethod_isVirtual(cursor) == 1)
-        return -1
-    end
-    ccall(("wci_getCXXMethodVTableIndex", :libwrapclang), Int32,
-		  (Ptr{Uint8},), cursor.data)
-end
-
-function method_mangled_name(cursor::cindex.CXCursor)
-    bufr = zeros(Uint8, 1024)
-    ccall(("wci_getCXXMethodMangledName", :libwrapclang), Int32,
-		 (Ptr{Uint8},Ptr{Uint8}), cursor.data, bufr)
-    bytestring(convert(Ptr{Uint8},bufr))
+	search(c, isa(c, cindex.CXXBaseSpecifier))
 end
 
 end # module wrap_cpp

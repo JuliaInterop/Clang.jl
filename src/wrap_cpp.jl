69d6cfffa006e9e761ed95eecfe48067d673f4e4
Isaiah Norton <isaiah.norton@gmail.com>
2013-09-28 14:54:34 -0400

    Add C gen part of new C++ wrapper generator
    Going to stub this out of module for now, for easier reloading.

--- src/wrap_cpp.jl
+++ src/wrap_cpp.jl
@@ -1,4 +1,4 @@
-module wrap_cpp
+# module wrap_cpp
 
 using Clang.cindex
 
@@ -6,4 +6,149 @@ function base_classes(c)
 	search(c, isa(c, cindex.CXXBaseSpecifier))
 end
 
-end # module wrap_cpp
+space(io::IO)      = print(io, " ")
+openparen(io::IO)  = print(io, "(")
+closeparen(io::IO) = print(io, ")")
+opencurly(io::IO)  = print(io, "{")
+closecurly(io::IO) = print(io, "}")
+opensquare(io::IO) = print(io, "[")
+closesquare(io::IO)= print(io, "]")
+comma(io::IO)      = print(io, ",")
+newline(io::IO)    = print(io, "\n")
+
+function emit(out::IO, t::cindex.CLType)
+    print(out, cl_to_c[typeof(t)])
+end
+
+function emit(out::IO, t::Union(cindex.Record, cindex.Typedef))
+    print(out, spelling(cindex.getTypeDeclaration(t)))
+end
+
+function emit(out::IO, t::cindex.ConstantArray)
+    arrtype = cindex.getArrayElementType(t)
+    arrsize = cindex.getArraySize(t)
+    emit(out, arrtype)
+    opensquare(out); print(out, arrsize); closesquare(out)
+end    
+
+function emit(out::IO, t::cindex.Pointer)
+    pointee = cindex.pointee_type(t)
+    emit(out, pointee)
+    print(out, "*")
+end
+
+function base_classes(c)
+	cindex.search(c, x->isa(x, cindex.CXXBaseSpecifier))
+end
+
+function emit(out::IO, parm::cindex.ParmDecl)
+    emit(out, cu_type(parm))
+end
+
+function check_args(args)
+    for arg in args
+        argt = cu_type(arg)
+        if isa(argt, cindex.LValueReference)  ||
+               isa(argt, cindex.FirstBuiltin)
+            return false
+        end
+    end
+    return true
+end
+
+function get_args(method::cindex.CXXMethod)
+    args = CLCursor[]
+    for c in children(method) isa(c,cindex.ParmDecl) ? push!(args, c) : break end
+    args
+end
+
+function emit_args(out::IO, args)
+    emit_arg(out,arg,docomma=false) = begin
+        emit(out,arg)
+        space(out)
+        print(out,spelling(arg))
+        docomma && (comma(out); space(out))
+    end
+    for i = 1:length(args)
+        emit_arg(out,args[i], i < length(args))
+    end
+end
+
+function emit_name(out::IO, method::cindex.CXXMethod, parent::cindex.CLCursor)
+    print(out, spelling(method))
+end
+
+function wrap(out::IO, method::cindex.CXXMethod)
+    buf = IOBuffer()
+    parentdecl = cindex.getCursorLexicalParent(method)
+    parentname = spelling(parentdecl)
+
+    # emit return type and name
+    ret_type = return_type(method)    
+    emit(buf, ret_type)
+    space(buf)
+    emit_name(buf, method, parentdecl)
+
+    # emit arguments
+    openparen(buf)
+    print(buf, parentname, "* this")
+    args = get_args(method)
+    if (!check_args(args)) return end
+    length(args) > 0 && (comma(buf); space(buf))
+    emit_args(buf, args)
+    closeparen(buf)
+    space(buf); opencurly(buf)
+    newline(buf)
+    
+    # emit method call
+    print(buf, "  this->", spelling(method))
+    openparen(buf)
+    emit_args(buf, args)
+    closeparen(buf)
+    
+    # close
+    newline(buf)
+    closecurly(buf)
+    newline(buf)
+
+    # print the buffer to the outpur
+    print(STDOUT, takebuf_string(buf))
+    # TODO: return information about the thing we wrapped?
+end
+
+function wrap(out::IO, top::cindex.ClassDecl)
+    println("Wrapping class: ", name(top))
+    for decl in [c for c in children(top)] #[1:30] # TODO: remove this
+        isa(decl, cindex.CXXMethod) && wrap(out, decl)
+    end
+end
+
+cl_to_c = {
+    cindex.VoidType       => "void",
+    cindex.FirstBuiltin   => "void",
+    cindex.BoolType       => "bool",
+    cindex.Char_U         => "unsigned char",
+    cindex.UChar          => "unsigned char",
+    cindex.Char16         => "char16_t",
+    cindex.Char32         => "char32_t",
+    cindex.UShort         => "unsigned short",
+    cindex.UInt           => "unsigned int",
+    cindex.ULong          => "unsigned long",
+    cindex.ULongLong      => "unsigned long long",
+    cindex.Char_S         => "char",
+    cindex.SChar          => "char",
+    cindex.WChar          => "wchar_t",
+    cindex.Short          => "short",
+    cindex.IntType        => "int",
+    cindex.Long           => "long",
+    cindex.LongLong       => "long long",
+    cindex.Float          => "float",
+    cindex.Double         => "double",
+    cindex.LongDouble     => "long double",
+    cindex.Enum           => "enum",
+    cindex.NullPtr        => "NULL",
+    cindex.UInt128        => "uint128_t"
+    }
+
+
+# end # module wrap_cpp

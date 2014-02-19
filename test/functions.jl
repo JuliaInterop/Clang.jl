d2dfcf56090e86f2d30f71b52d5e8a96a019d080
Joosep Pata <joosep.pata@gmail.com>
2014-02-19 13:50:52 +0100

    fixes for cpp, checking of return type modfiers, generation on c/jl wrappers for cpp

---
+++ test/functions.jl
@@ -0,0 +1,22 @@
+using Clang.cindex
+using Base.Test
+
+top = cindex.parse_header("cxx/cbasic.h";cplusplus = true)
+
+funcs = cindex.search(top, "func")
+@test length(funcs) == 1
+f = funcs[1]
+
+#@test map(spelling, cindex.function_args(f)) == ASCIIString["Int", "Int"]
+@test map(spelling, cindex.function_args(f)) == ASCIIString["x", "y"]
+@test isa(return_type(f), IntType)
+
+defs = cindex.function_arg_defaults(f)
+@test defs == (nothing, 1)
+
+
+funcs = cindex.search(top, "func2")
+@test length(funcs) == 1
+f = funcs[1]
+
+@test all(map(x->x.text, function_return_modifiers(f)) .== ["const", ])

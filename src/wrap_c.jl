edde4de2ccef7758e0e8923c9d2be551fddfba98
Isaiah Norton <isaiah.norton@gmail.com>
2014-05-27 00:59:32 -0400

    Handle floating-point literal suffix

--- src/wrap_c.jl
+++ src/wrap_c.jl
@@ -429,7 +429,7 @@ function lex_exprn(tokens::TokenList, pos::Int)
         # note: put multi-character first, or it will break out too soon for those!
         literalsuffixes = ["ULL", "Ull", "uLL", "ull", "LLU", "LLu", "llU", "llu",
                            "LL", "ll", "UL", "Ul", "uL", "ul", "LU", "Lu", "lU", "lu",
-                           "U", "u", "L", "l"]
+                           "U", "u", "L", "l", "F", "f"]
         txt = tok.text
         if isa(tok,cindex.Identifier) || isa(tok,cindex.Punctuation)
             # pass

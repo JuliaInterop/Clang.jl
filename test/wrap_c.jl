5c2f644b46d36e1431853c207dba70629fb102a9
Tim Holy <tim.holy@gmail.com>
2015-01-12 21:17:27 -0600

    Turn on tests in runtests.jl, and fix expr comparison in test/wrap_c

--- test/wrap_c.jl
+++ test/wrap_c.jl
@@ -11,9 +11,25 @@ buf = Any[]
 
 wrap_c.wrap(wc, buf, func1, "test")
 
-@test buf[1] == quote
+exc = :(
     function func1(a::Cint,b::Cdouble,c::Ptr{Cdouble},d::Ptr{Void})
         ccall((:func1,test),Cint,(Cint,Cdouble,Ptr{Cdouble},Ptr{Void}),a,b,c,d)
+    end)
+
+strip_line_numbers!(arg) = arg
+function strip_line_numbers!(ex::Expr)
+    keep = trues(length(ex.args))
+    for i = 1:length(ex.args)
+        isa(ex.args[i], Expr) || continue
+        if ex.args[i].head == :line
+            keep[i] = false
+        else
+            strip_line_numbers!(ex.args[i])
+        end
     end
+    ex.args = ex.args[keep]
 end
 
+strip_line_numbers!(exc)
+
+@test buf[1] == exc

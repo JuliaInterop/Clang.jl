fb6c38a416550a73e653792d2480abae257464bf
Isaiah Norton <isaiah.norton@gmail.com>
2015-01-24 23:58:43 -0500

    fix and generalize llvm-config detection
    fixes #114

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,17 @@
-ENV["JULIA_ROOT"] = abspath(JULIA_HOME, "../../")
+find_llvm() = begin
+    if haskey(ENV, "TRAVIS")
+        return readchomp(`which llvm-config-3.3`)
+    end
+    tmp = joinpath(JULIA_HOME, "llvm-config")
+    isfile(tmp) && return tmp
+    tmp = readchomp(`which llvm-config`)
+    isfile(tmp) && return tmp
+end
+if !haskey(ENV, "LLVM_CONFIG") ENV["LLVM_CONFIG"] = find_llvm() end
+
+# on travis and some other systems, the llvm lib may not be in the
+# default path.
+push!(DL_LOAD_PATH, readchomp(`$(ENV["LLVM_CONFIG"]) --libdir`))
 
 cd(joinpath(Pkg.dir(), "Clang", "deps", "src") )
 run(`make`)
@@ -8,4 +21,5 @@ end
 if (!ispath("../usr/lib")) 
   run(`mkdir ../usr/lib`)
 end
+
 run(`mv libwrapclang.$(Base.Sys.shlib_ext) ../usr/lib`)
--- deps/ext.jl
+++ deps/ext.jl
@@ -1,2 +1,7 @@
-const libwci = find_library(["libwrapclang",],[Pkg.dir("Clang", "deps", "usr", "lib"),])
-@assert(libwci != "", "Failed to find required library libwrapclang. Try re-running the package script using Pkg.build(\"Clang\")")
+if haskey(ENV, "TRAVIS")
+    push!(DL_LOAD_PATH, "/usr/lib/llvm-3.3/lib")
+    const libwci = joinpath(Pkg.dir("Clang","deps","usr","lib"), "libwrapclang.so")
+else
+    const libwci = find_library(["libwrapclang",],[Pkg.dir("Clang", "deps", "usr", "lib"),])
+    @assert(libwci != "", "Failed to find required library libwrapclang. Try re-running the package script using Pkg.build(\"Clang\")")
+end

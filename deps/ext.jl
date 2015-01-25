c4d18994e7b4f6a5d8de6f88eea681639528a74a
Isaiah Norton <isaiah.norton@gmail.com>
2015-01-25 10:42:05 -0500

    debugging

--- deps/ext.jl
+++ deps/ext.jl
@@ -5,3 +5,7 @@ else
     const libwci = find_library(["libwrapclang",],[Pkg.dir("Clang", "deps", "usr", "lib"),])
     @assert(libwci != "", "Failed to find required library libwrapclang. Try re-running the package script using Pkg.build(\"Clang\")")
 end
+
+println("libwci: ", libwci)
+println("DLLP: ", DL_LOAD_PATH)
+run(`bash -c "ls /usr/lib/llvm-3.3/lib/libclang*"`)

03990614f0d10f9104017d428c4c3a175d3152d3
Yichao Yu <yyc1992@gmail.com>
2015-10-01 21:54:57 -0400

    Revert d8e5bf74406d41b1b65dda9731851e6f996afb35 on deps/ext.jl. There was no point of checking v0.4 or breaks installation in LOAD_PATH

--- deps/ext.jl
+++ deps/ext.jl
@@ -1,13 +1,10 @@
-
 if haskey(ENV, "TRAVIS")
     push!(Libdl.DL_LOAD_PATH, "/usr/lib/llvm-3.3/lib")
     const libwci = joinpath(dirname(@__FILE__),
                             "usr", "lib" , "libwrapclang.so")
 else
-  if VERSION > v"0.4-"
-    const libwci = Base.Libdl.find_library(["libwrapclang",],[Pkg.dir("Clang", "deps", "usr", "lib"),])
-  else
-    const libwci = find_library(["libwrapclang",],[Pkg.dir("Clang", "deps", "usr", "lib"),])
-  end
+    const libwci = Libdl.find_library(["libwrapclang",],
+                                      [joinpath(dirname(@__FILE__),
+                                                "usr", "lib")])
     @assert(libwci != "", "Failed to find required library libwrapclang. Try re-running the package script using Pkg.build(\"Clang\")")
 end

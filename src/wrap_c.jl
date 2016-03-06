a526e9ff0c2c41945e4577377fb8da48fbc82a8b
Valentin Churavy <v.churavy@gmail.com>
2016-03-06 22:51:09 +0900

    optimistically create unions where all file sizes are unkown

--- src/wrap_c.jl
+++ src/wrap_c.jl
@@ -322,6 +322,11 @@ function largestfield(cu::UnionDecl)
             maxelem = i
         end
     end
+    if maxelem == 0
+        warn("  all field sizes are unknown.")
+        maxelem = 1
+    end
+
     fields[maxelem]
 end
 

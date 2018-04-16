a1c93fd6ff4d81bdcb404091a7da510495892194
Isaiah Norton <isaiah.norton@gmail.com>
2018-04-15 23:26:23 -0400

    Remove shim builder code

--- deps/ext.jl
+++ deps/ext.jl
@@ -5,6 +5,3 @@ include("deps.jl")
 push!(Libdl.DL_LOAD_PATH, readchomp(`$(LLVM_CONFIG) --libdir`))
 
 Libdl.dlopen("libclang", Libdl.RTLD_DEEPBIND)
-
-const libwci = joinpath(dirname(@__FILE__), "usr", "lib", "libwrapclang." * Libdl.dlext)
-@assert(libwci != "", "Failed to find required library libwrapclang. Try re-running the package script using Pkg.build(\"Clang\")")

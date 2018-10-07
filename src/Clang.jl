25e7a7922e2cb83eb4b529ce3ce5354b44b7cbb8
Gnimuc <qiyupei@gmail.com>
2018-10-07 20:53:25 +0800

    Add CEnum.jl and misc. code update

--- src/Clang.jl
+++ src/Clang.jl
@@ -1,22 +1,42 @@
 module Clang
 
+import Libdl
+using DataStructures
+
 # Load in `deps.jl`, complaining if it does not exist
 const depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
 if !isfile(depsjl_path)
     error("Clang was not build properly. Please run Pkg.build(\"Clang\").")
 end
 include(depsjl_path)
+
+include("CEnum.jl")
+using .CEnum
+
+include(joinpath(@__DIR__, "..", "gen", "libclang_common.jl"))
+include(joinpath(@__DIR__, "..", "gen", "libclang_api.jl"))
+
+## visitor
+function cu_children_visitor(cursor::CXCursor, parent::CXCursor, client_data::Ptr{Cvoid})
+    list = unsafe_pointer_to_objref(client_data)
+    push!(list, cursor)
+    return CXChildVisit_Continue
+end
+
 # Module initialization function
 function __init__()
     check_deps()
+    global cu_visitor_cb = @cfunction(cu_children_visitor, Cuint, (CXCursor, CXCursor, Ptr{Cvoid}))
 end
 
+include("base.jl")
 include("cindex.jl")
-include("wrap_c.jl")
-include("wrap_cpp.jl")
+export parse_header, search, resolve_type, return_type, typedef_type, value, function_args
+export children, cu_visitor_cb
 
-export cindex
-export wrap_c
-export wrap_cpp
+include("constants.jl")
+include("wrap_c.jl")
+export wrap_c_headers
+export WrapContext
 
 end

161d38bc47e220a0ea4524d7b17a7e5a2ab86a3c
Gnimuc <qiyupei@gmail.com>
2018-11-17 15:35:36 +0800

    Make trans_units a vector for easier indexing.

--- test/macro.jl
+++ test/macro.jl
@@ -1,15 +1,16 @@
 using Clang
+using Clang.LibClang
 using Test
 
 @testset "macro" begin
     trans_unit = parse_header(joinpath(@__DIR__, "c", "macro.h"),
-                                  flags = CXTranslationUnit_DetailedPreprocessingRecord |
-                                          CXTranslationUnit_SkipFunctionBodies)
+                              flags = CXTranslationUnit_DetailedPreprocessingRecord |
+                                      CXTranslationUnit_SkipFunctionBodies)
     root_cursor = getcursor(trans_unit)
     cursors = children(root_cursor)
     g_uri_xxx = search(cursors, x->name(x)=="G_URI_RESERVED_CHARS_SUBCOMPONENT_DELIMITERS")[1]
     ctx = DefaultContext()
-    ctx.trans_units["macro.h"] = trans_unit
+    push!(ctx.trans_units, trans_unit)
     wrap!(ctx, g_uri_xxx)
     expr = :(const G_URI_RESERVED_CHARS_SUBCOMPONENT_DELIMITERS = "!\$&'()*+,;=")
     @test ctx.common_buffer[:G_URI_RESERVED_CHARS_SUBCOMPONENT_DELIMITERS].items[1] == expr

ab1295e3622d2208829dd7ee297d8176bb30b318
Gnimuc <qiyupei@gmail.com>
2018-12-31 15:00:24 +0800

    Improve the sanity of `tokenize`
    just tokenize the macro callsite, not the whole file.

--- test/c/macro.h
+++ test/c/macro.h
@@ -1,1 +1,8 @@
 #define G_URI_RESERVED_CHARS_SUBCOMPONENT_DELIMITERS "!$&'()*+,;="
+
+#define FOO(x, a, b) (a) <= (x) && (x) < (b)
+void macro_range(void) {
+    int foo = 1;
+    int x = 0;
+    x = FOO(foo, 1, x);
+}

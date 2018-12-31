af5a12338e0f0fc9b405373467456e244768d773
Gnimuc <qiyupei@gmail.com>
2018-12-31 15:00:24 +0800

    Improve the sanity of `tokenize`
    just tokenize the macro callsite, not the whole file.

--- test/macro.jl
+++ test/macro.jl
@@ -4,8 +4,7 @@ using Test
 
 @testset "macro" begin
     trans_unit = parse_header(joinpath(@__DIR__, "c", "macro.h"),
-                              flags = CXTranslationUnit_DetailedPreprocessingRecord |
-                                      CXTranslationUnit_SkipFunctionBodies)
+                              flags = CXTranslationUnit_DetailedPreprocessingRecord)
     root_cursor = getcursor(trans_unit)
     cursors = children(root_cursor)
     g_uri_xxx = search(cursors, x->name(x)=="G_URI_RESERVED_CHARS_SUBCOMPONENT_DELIMITERS")[1]
@@ -14,4 +13,10 @@ using Test
     wrap!(ctx, g_uri_xxx)
     expr = :(const G_URI_RESERVED_CHARS_SUBCOMPONENT_DELIMITERS = "!\$&'()*+,;=")
     @test ctx.common_buffer[:G_URI_RESERVED_CHARS_SUBCOMPONENT_DELIMITERS].items[1] == expr
+    # test tokenize for macro instantiation
+    func = search(cursors, x->kind(x)==CXCursor_FunctionDecl)[1]
+    body = children(func)[1]
+    op = children(body)[3]
+    subop = children(op)[2]
+    @test mapreduce(x->x.text, *, tokenize(subop)) == "FOO(foo,1,x)"
 end

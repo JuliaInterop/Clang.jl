207a14f0f8170a39ba7e963c0f05b73749de9772
4thjuly <4thjuly@ellison-taylor.com>
2020-12-19 12:39:36 -1000

    Add test

--- test/macro.jl
+++ test/macro.jl
@@ -22,4 +22,9 @@ using Test
     @test mapreduce(x->x.text, *, tokenize(subop)) == "FOO(foo,1,x)"
     wrap!(ctx, func)
     @test !isempty(ctx.api_buffer)
+
+    # Issue #270 Support '==' in macros
+    equals_macro = search(cursors, x->name(x)=="EQUALS_A_B")[1]
+    wrap!(ctx, equals_macro)
+    @test ctx.common_buffer[:EQUALS_A_B].items[1] == :(const EQUALS_A_B = A == B)
 end

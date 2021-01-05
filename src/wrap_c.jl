0f322d1160f50d8d900554a47fd688cf53fe3112
Gnimuc <qiyupei@gmail.com>
2021-01-05 22:18:11 +0900

    less closures

--- src/wrap_c.jl
+++ src/wrap_c.jl
@@ -323,61 +323,62 @@ function wrap!(ctx::AbstractContext, cursor::CLTypedefDecl)
     return ctx
 end
 
+## macros
+function trans(tok)
+    ops = ["+" "-" "*" "~" ">>" "<<" "/" "\\" "%" "|" "||" "^" "&" "&&" "=="]
+    token_kind = kind(tok)
+    (token_kind == CXToken_Literal || token_kind == CXToken_Identifier) && return 0
+    token_kind == CXToken_Punctuation && tok.text ∈ ops && return 1
+    return -1
+end
+
+# normalize literal with a size suffix
+function literally(tok)
+    # note: put multi-character first, or it will break out too soon for those!
+    literalsuffixes = ["ULL", "Ull", "uLL", "ull", "LLU", "LLu", "llU", "llu",
+                       "LL", "ll", "UL", "Ul", "uL", "ul", "LU", "Lu", "lU", "lu",
+                       "U", "u", "L", "l", "F", "f"]
+
+    function literal_totype(literal, txt)
+        literal = lowercase(literal)
+
+        # Floats following http://en.cppreference.com/w/cpp/language/floating_literal
+        float64 = occursin(".", txt) && occursin("l", literal)
+        float32 = occursin("f", literal)
+
+        if float64 || float32
+            float64 && return "Float64"
+            float32 && return "Float32"
+        end
+
+        # Integers following http://en.cppreference.com/w/cpp/language/integer_literal
+        unsigned = occursin("u", literal)
+        nbits = count(x -> x == 'l', literal) == 2 ? 64 : 32
+        return "$(unsigned ? "U" : "")Int$nbits"
+    end
+
+    token_kind = kind(tok)
+    txt = strip(tok.text)
+    if token_kind == CXToken_Identifier || token_kind == CXToken_Punctuation
+        # pass
+    elseif token_kind == CXToken_Literal
+        for sfx in literalsuffixes
+            if endswith(txt, sfx)
+                type = literal_totype(sfx, txt)
+                txt = txt[1:(end - length(sfx))]
+                txt = "$(type)($txt)"
+                break
+            end
+        end
+    end
+    return txt
+end
+
 """
     handle_macro_exprn(tokens::TokenList, pos::Int)
 For handling of #define'd constants, allows basic expressions but bails out quickly.
 """
 function handle_macro_exprn(tokens::TokenList, pos::Int)
-    function trans(tok)
-        ops = ["+" "-" "*" "~" ">>" "<<" "/" "\\" "%" "|" "||" "^" "&" "&&" "=="]
-        token_kind = kind(tok)
-        (token_kind == CXToken_Literal || token_kind == CXToken_Identifier) && return 0
-        token_kind == CXToken_Punctuation && tok.text ∈ ops && return 1
-        return -1
-    end
-
-    # normalize literal with a size suffix
-    function literally(tok)
-        # note: put multi-character first, or it will break out too soon for those!
-        literalsuffixes = ["ULL", "Ull", "uLL", "ull", "LLU", "LLu", "llU", "llu",
-                           "LL", "ll", "UL", "Ul", "uL", "ul", "LU", "Lu", "lU", "lu",
-                           "U", "u", "L", "l", "F", "f"]
-
-        function literal_totype(literal, txt)
-            literal = lowercase(literal)
-
-            # Floats following http://en.cppreference.com/w/cpp/language/floating_literal
-            float64 = occursin(".", txt) && occursin("l", literal)
-            float32 = occursin("f", literal)
-
-            if float64 || float32
-                float64 && return "Float64"
-                float32 && return "Float32"
-            end
-
-            # Integers following http://en.cppreference.com/w/cpp/language/integer_literal
-            unsigned = occursin("u", literal)
-            nbits = count(x -> x == 'l', literal) == 2 ? 64 : 32
-            return "$(unsigned ? "U" : "")Int$nbits"
-        end
-
-        token_kind = kind(tok)
-        txt = strip(tok.text)
-        if token_kind == CXToken_Identifier || token_kind == CXToken_Punctuation
-            # pass
-        elseif token_kind == CXToken_Literal
-            for sfx in literalsuffixes
-                if endswith(txt, sfx)
-                    type = literal_totype(sfx, txt)
-                    txt = txt[1:(end - length(sfx))]
-                    txt = "$(type)($txt)"
-                    break
-                end
-            end
-        end
-        return txt
-    end
-
     # check whether identifiers and literals alternate
     # with punctuation
     exprn = ""

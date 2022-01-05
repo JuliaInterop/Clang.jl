b76e9ae49b5ccb0316f9d1d61ece5a3ee545b258
melonedo <44501064+melonedo@users.noreply.github.com>
2022-01-05 12:53:50 +0800

    Call library function after build
    This ensures the binary is callable.

--- test/test_bitfield.jl
+++ test/test_bitfield.jl
@@ -2,48 +2,54 @@ using CMake
 using Clang.Generators
 
 function build_libbitfield()
-    @info "Building libbitfield. This will break if CMake fails to find a C compiler"
-    src_dir = joinpath(@__DIR__, "bitfield")
-    build_dir = joinpath(@__DIR__, "build", "bitfield")
+    success = true
+    try
+        # Compile binary
+        @info "Building libbitfield binary. This will break if CMake fails to find a C compiler"
+        src_dir = joinpath(@__DIR__, "bitfield")
+        build_dir = joinpath(@__DIR__, "build", "bitfield")
 
-    config_cmd = `$cmake -B $build_dir -S $src_dir`
-    if Sys.WORD_SIZE == 32
-        if Sys.iswindows()
-            config_cmd = `$config_cmd -A win32`
-        elseif Sys.islinux()
-            config_cmd = `$config_cmd -D CMAKE_C_FLAGS=-march=native -D CMAKE_CXX_FLAGS=-march=native`
+        config_cmd = `$cmake -B $build_dir -S $src_dir`
+        if Sys.WORD_SIZE == 32
+            if Sys.iswindows()
+                config_cmd = `$config_cmd -A win32`
+            elseif Sys.islinux()
+                config_cmd = `$config_cmd -D CMAKE_C_FLAGS=-march=native -D CMAKE_CXX_FLAGS=-march=native`
+            end
         end
+        build_cmd = `$cmake --build $build_dir --config Debug`
+        install_cmd = `$cmake --install $build_dir --config Debug --prefix $build_dir`
+
+        run(config_cmd)
+        run(build_cmd)
+        run(install_cmd)
+
+        # Generate wrappers
+        @info "Building libbitfield wrapper"
+        args = get_default_args()
+        headers = joinpath(build_dir, "include", "bitfield.h")
+        options = load_options(joinpath(src_dir, "generate.toml"))
+        lib_path = joinpath(build_dir, "bin", Sys.iswindows() ? "bitfield.dll" : "libbitfield")
+        options["general"]["library_name"] = "\"$(escape_string(lib_path))\""
+        options["general"]["output_file_path"] = joinpath(@__DIR__, "LibBitField.jl")
+        ctx = create_context(headers, args, options)
+        build!(ctx)
+
+        # Call a function to ensure build is successful
+        include("LibBitField.jl")
+        m = Base.@invokelatest LibBitField.Mirror(10, 1.5, 1e6, -4, 7, 3)
+        Base.@invokelatest LibBitField.toBitfield(Ref(m))
+    catch e
+        @warn "Building libbitfield failed: $e"
+        success = false
     end
-    build_cmd = `$cmake --build $build_dir --config Debug`
-    install_cmd = `$cmake --install $build_dir --config Debug --prefix $build_dir`
-
-    run(config_cmd)
-    run(build_cmd)
-    run(install_cmd)
-
-    @info "Building libbitfield wrapper"
-    args = get_default_args()
-    headers = joinpath(build_dir, "include", "bitfield.h")
-    options = load_options(joinpath(src_dir, "generate.toml"))
-    lib_path = joinpath(build_dir, "bin", Sys.iswindows() ? "bitfield.dll" : "libbitfield")
-    options["general"]["library_name"] = "\"$(escape_string(lib_path))\""
-    options["general"]["output_file_path"] = joinpath(@__DIR__, "LibBitField.jl")
-    ctx = create_context(headers, args, options)
-    build!(ctx)
+    return success
 end
 
 
 
 @testset "Bitfield" begin
-    failed_to_build_bitfield = false
-    try
-        build_libbitfield()
-    catch e
-        failed_to_build_bitfield = true
-        @info "Failed to build library libbitfield, skipping bitfield tests"
-    end
-    if !failed_to_build_bitfield
-        include("LibBitField.jl")
+    if build_libbitfield()
         bf = Ref(LibBitField.BitField(Int8(10), 1.5, Int32(1e6), Int32(-4), Int32(7), UInt32(3)))
         m = Ref(LibBitField.Mirror(10, 1.5, 1e6, -4, 7, 3))
         GC.@preserve bf m begin

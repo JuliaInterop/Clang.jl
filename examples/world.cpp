0c713e7e458dd7f0d29b0b3dc451500e596852a8
Gnimuc <qiyupei@gmail.com>
2022-01-24 23:34:32 +0900

    Add CxxWrap.jl's class example source code
    Co-Authored-By: Paul Berg <9824244+Pangoraw@users.noreply.github.com>

---
+++ examples/world.cpp
@@ -0,0 +1,10 @@
+#include <string>
+
+struct World
+{
+  World(const std::string& message = "default hello") : msg(message){}
+  void set(const std::string& msg) { this->msg = msg; }
+  std::string greet() { return msg; }
+  std::string msg;
+  ~World() { std::cout << "Destroying World with message " << msg << std::endl; }
+};

dcad567e67f1d8181367842491402534745f15ec
Gnimuc <qiyupei@gmail.com>
2022-01-28 15:00:39 +0900

    Use ClangCompiler

--- test/test_bitfield.jl
+++ test/test_bitfield.jl
@@ -1,67 +1,62 @@
-using CMake
+using ClangCompiler
+using ClangCompiler.LLVM
 using Clang.Generators
+using Test
 
-function build_libbitfield()
-    success = true
-    try
-        # Compile binary
-        @info "Building libbitfield binary. This will break if CMake fails to find a C compiler"
-        src_dir = joinpath(@__DIR__, "bitfield")
-        build_dir = joinpath(@__DIR__, "build", "bitfield")
+# Compile
+src = joinpath(@__DIR__, "bitfield", "bitfield.c")
+args = get_compiler_args()
+jit = LLJIT(; tm = JITTargetMachine())
+irgen = IRGenerator(src, args)
+cc = CXCompiler(irgen, jit)
+link_process_symbols(cc)
+compile(cc)
 
-        config_cmd = `$cmake -B $build_dir -S $src_dir`
-        if Sys.WORD_SIZE == 32
-            if Sys.iswindows()
-                config_cmd = `$config_cmd -A win32`
-            elseif Sys.islinux()
-                config_cmd = `$config_cmd -D CMAKE_C_FLAGS=-march=native -D CMAKE_CXX_FLAGS=-march=native`
+options = Dict("general" => Dict(), "codegen" => Dict())
+options["general"]["output_file_path"] = joinpath(@__DIR__, "LibBitField.jl")
+options["codegen"]["add_record_constructors"] = true
+ctx = create_context(joinpath(@__DIR__, "bitfield", "bitfield.h"), args, options)
+
+build!(ctx, BUILDSTAGE_NO_PRINTING)
+
+function rewrite!(dag::ExprDAG)
+    for node in get_nodes(dag)
+        node.type isa Generators.AbstractFunctionNodeType || continue
+        func_name = string(node.id)
+        handle = Symbol(func_name * "_handle")
+        for expr in node.exprs
+            Meta.isexpr(expr, :function) || continue
+            for block_expr in expr.args
+                Meta.isexpr(block_expr, :block) || continue
+                for ccall_expr in block_expr.args
+                    Meta.isexpr(ccall_expr, :call) || continue
+                    handle_expr = :(pointer(lookup(jit, $func_name)))
+                    ccall_expr.args[2] = handle_expr
+                end
             end
         end
-        build_cmd = `$cmake --build $build_dir --config Debug`
-        install_cmd = `$cmake --install $build_dir --config Debug --prefix $build_dir`
-
-        run(config_cmd)
-        run(build_cmd)
-        run(install_cmd)
-
-        # Generate wrappers
-        @info "Building libbitfield wrapper"
-        args = get_default_args()
-        headers = joinpath(build_dir, "include", "bitfield.h")
-        options = load_options(joinpath(src_dir, "generate.toml"))
-        lib_path = joinpath(build_dir, "bin", Sys.iswindows() ? "bitfield.dll" : "libbitfield")
-        options["general"]["library_name"] = "\"$(escape_string(lib_path))\""
-        options["general"]["output_file_path"] = joinpath(@__DIR__, "LibBitField.jl")
-        ctx = create_context(headers, args, options)
-        build!(ctx)
-
-        # Call a function to ensure build is successful
-        include("LibBitField.jl")
-        m = Base.@invokelatest LibBitField.Mirror(10, 1.5, 1e6, -4, 7, 3)
-        Base.@invokelatest LibBitField.toBitfield(Ref(m))
-    catch e
-        @warn "Building libbitfield failed: $e"
-        success = false
     end
-    return success
 end
 
+rewrite!(ctx.dag)
+build!(ctx, BUILDSTAGE_PRINTING_ONLY)
 
+include("LibBitField.jl")
 
 @testset "Bitfield" begin
-    if build_libbitfield()
-        bf = Ref(LibBitField.BitField(Int8(10), 1.5, Int32(1e6), Int32(-4), Int32(7), UInt32(3)))
-        m = Ref(LibBitField.Mirror(10, 1.5, 1e6, -4, 7, 3))
-        GC.@preserve bf m begin
-            pbf = Ptr{LibBitField.BitField}(pointer_from_objref(bf))
-            pm = Ptr{LibBitField.Mirror}(pointer_from_objref(m))
-            @test LibBitField.toMirror(bf) == m[]
-            @test LibBitField.toBitfield(m).a == bf[].a
-            @test LibBitField.toBitfield(m).b == bf[].b
-            @test LibBitField.toBitfield(m).c == bf[].c
-            @test LibBitField.toBitfield(m).d == bf[].d
-            @test LibBitField.toBitfield(m).e == bf[].e
-            @test LibBitField.toBitfield(m).f == bf[].f
-        end
+    bf = Ref(BitField(Int8(10), 1.5, Int32(1e6), Int32(-4), Int32(7), UInt32(3)))
+    m = Ref(Mirror(10, 1.5, 1e6, -4, 7, 3))
+    GC.@preserve cc bf m begin
+        pbf = Ptr{BitField}(pointer_from_objref(bf))
+        pm = Ptr{Mirror}(pointer_from_objref(m))
+        @test toMirror(bf) == m[]
+        @test toBitfield(m).a == bf[].a
+        @test toBitfield(m).b == bf[].b
+        @test toBitfield(m).c == bf[].c
+        @test toBitfield(m).d == bf[].d
+        @test toBitfield(m).e == bf[].e
+        @test toBitfield(m).f == bf[].f
     end
 end
+
+dispose(cc)

JuliaInterop/Clang.jl

commit a6011b36137e19c6a4df36c90435cc5a83282743

Generate `Base.unsafe_convert` methods for `TypedefMutualRef`s
We need these `unsafe_convert` methods for converting ccall argtypes where the fake struct type is being used.

src/generator/codegen.jl
@@ -234,6 +234,10 @@ function emit!(dag::ExprDAG, node::ExprNode{TypedefMutualRef}, options::Dict; ar
         signature = Expr(:call, :(Base.setproperty!), :(x::Ptr{$fake_sym}), :(f::Symbol), :v)
         body = Expr(:block, :(setproperty!(Ptr{$real_sym}(x), f, v)))
         push!(node.exprs, Expr(:function, signature, body))
+        # `Base.unsafe_convert`
+        lhs = Expr(:call, :(Base.unsafe_convert), :(::Type{Ptr{$fake_sym}}), :(x::Ref))
+        rhs = :(Base.unsafe_convert(Ptr{$fake_sym}, Base.unsafe_convert(Ptr{$real_sym}, x)))
+        push!(node.exprs, :($lhs = $rhs))
 
         # generate typedef
         typedefee = translate(jlty, options)

commit 073ffafad73ae574c5a7b7756733483d32cfb9e1

Fix the ambiguous issue introduced in a6011b36

src/generator/codegen.jl
@@ -238,6 +238,10 @@ function emit!(dag::ExprDAG, node::ExprNode{TypedefMutualRef}, options::Dict; ar
         lhs = Expr(:call, :(Base.unsafe_convert), :(::Type{Ptr{$fake_sym}}), :(x::Ref))
         rhs = :(Base.unsafe_convert(Ptr{$fake_sym}, Base.unsafe_convert(Ptr{$real_sym}, x)))
         push!(node.exprs, :($lhs = $rhs))
+        # make sure the behavior remains the same for `Ptr`
+        lhs = Expr(:call, :(Base.unsafe_convert), :(::Type{Ptr{$fake_sym}}), :(x::Ptr))
+        rhs = :(Ptr{$fake_sym}(x))
+        push!(node.exprs, :($lhs = $rhs))
 
         # generate typedef
         typedefee = translate(jlty, options)
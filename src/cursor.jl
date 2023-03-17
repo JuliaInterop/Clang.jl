fde4a016f5ac5fe81287e7f32a30a8fd2b060fb8
Nehal Patel <nehal@alum.mit.edu>
2023-03-17 15:49:40 -0400

    Move `_cxstring_to_string` out of `cursor.jl`.

--- src/cursor.jl
+++ src/cursor.jl
@@ -18,15 +18,6 @@ Base.hash(x::CLCursor) = hash(x.cursor)
 Base.:(==)(c1::CLCursor, c2::CXCursor) = c1.cursor == c2
 Base.:(==)(c1::CXCursor, c2::CLCursor) = c1 == c2.cursor
 
-"Free and convert a CXString to String. Note this function will free the given CXString so ensure it is not called twice."
-function _cxstring_to_string(cxstr::CXString)
-    ptr = clang_getCString(cxstr)
-    ptr == C_NULL && return ""
-    s = unsafe_string(ptr)
-    clang_disposeString(cxstr)
-    return s
-end
-
 """
     kind(c::CXCursor) -> CXCursorKind
 Return the kind of the given cursor.

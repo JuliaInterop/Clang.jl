bdb93d892c51d09b8b0c53d4dfac88ad3a1bbf36
James Wrigley <JamesWrigley@users.noreply.github.com>
2023-12-29 02:52:29 +0100

    Add support for Doxygen's @deprecated command (#460)
    Also add a simple test for the documentation parser.

---
+++ test/include/documentation.h
@@ -0,0 +1,14 @@
+/**
+ * @brief Dummy function.
+ *
+ * @param foo           A parameter.
+ *
+ * @return              Whatever I want.
+ *
+ * @see quux()
+ *
+ * @deprecated This function is evil.
+ */
+void doxygen_func(int foo) {
+    return 0;
+}

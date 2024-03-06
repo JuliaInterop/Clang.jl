c22d3bb648fbc4c8c154e05c5345f1d05b594c44
JamesWrigley <james@puiterwijk.org>
2024-03-06 14:20:01 +0100

    Load the REPL module in the tests so we can use @docs
    See: https://github.com/JuliaLang/julia/issues/52986

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using Clang
 using Test
 
+# Temporary hack to make @doc work in 1.11 for the documentation tests. See:
+# https://github.com/JuliaLang/julia/issues/52986
+using REPL
+
 include("generators.jl")
 
 include("test_mpi.jl")

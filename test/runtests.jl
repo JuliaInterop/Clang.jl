45d8130393290c756da48e1942a73ce87232011e
JamesWrigley <james@puiterwijk.org>
2024-08-20 17:59:39 +0200

    Revert "Remove test workaround (#504)"
    This reverts commit 9c77fd6b4982c8b34c21a64748aea2664409206e. Now the tests fail
    for a different reason :')

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using Clang
 using Test
 
+# Temporary hack to make @doc work in 1.11 for the documentation tests. See:
+# https://github.com/JuliaLang/julia/issues/54664
+using REPL
+
 include("jllenvs.jl")
 include("file.jl")
 include("generators.jl")

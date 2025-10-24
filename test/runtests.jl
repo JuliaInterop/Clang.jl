08e5db5fe8807c4969c4bce87382945d77d39ce0
JamesWrigley <james@puiterwijk.org>
2025-10-24 20:52:01 +0200

    Disable ReTest.jl
    This is currently broken on nightly.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,18 @@
-import ReTest: retest
+using Test
 import Clang
 
 # Temporary hack to make @doc work in 1.11 for the documentation tests. See:
 # https://github.com/JuliaLang/julia/issues/54664
 using REPL
 
-include("ClangTests.jl")
+include("jllenvs.jl")
+include("file.jl")
+include("generators.jl")
+include("module.jl")
 
-retest(Clang, ClangTests; stats=true)
+include("test_mpi.jl")
+include("test_bitfield.jl")
+
+# ReTest.jl is disabled for now because it doesn't support Julia 1.13
+# include("ClangTests.jl")
+# retest(Clang, ClangTests; stats=true)
